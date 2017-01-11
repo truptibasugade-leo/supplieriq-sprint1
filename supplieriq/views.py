dd9c7e8d7c6260b7df587a3b748a974c4a210bce
trupti <truptib@leotechnosoft.net>
2017-01-11 18:07:17 +0530

    Login API

--- supplieriq/views.py
+++ supplieriq/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+import json
+import datetime
+from django.utils import timezone
+from django.contrib.auth import\
+    get_user_model, authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework import parsers, renderers, status
+from rest_framework.authentication import get_authorization_header
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authentication import TokenAuthentication,SessionAuthentication
+from django.middleware.csrf import rotate_token
+from django.contrib import messages
+import time
+from django.utils.http import cookie_date
+from django.core.cache import cache
+from django.contrib.auth.models import Group
+from supplieriq.serializers import SignInSerializer
+from django.contrib.auth.models import User
+from django.contrib import auth
+from rest_framework.renderers import TemplateHTMLRenderer
 
-# Create your views here.
+
+class ObtainAuthToken(APIView):
+
+    """
+    Constraints:
+    This API is not for mobile Team.
+    """
+
+    throttle_classes = ()
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (AllowAny,)
+    parser_classes = (
+        parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,TemplateHTMLRenderer)
+    serializer_class = SignInSerializer  # AuthTokenSerializer
+    model = Token
+    template_name = 'login.html'
+    
+    def get(self, request):
+        serializer = self.serializer_class()
+        if request.user.is_authenticated():
+            token, created = Token.objects.get_or_create(user=request.user)  
+            return Response({'serializer': serializer,'token': token.key,'userid': request.user.id,}, template_name='home.html')
+        
+        return Response({'serializer': serializer})
+    
+    # Accepte un backend en parametre : 'auth' pour un login / pass classique
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)        
+        if serializer.is_valid():
+            user = serializer.instance            
+            token, created = Token.objects.get_or_create(user=user)  
+#             auth_login(request, user)
+            new_user = authenticate(username=user.username, password=request.data.get('password'))
+            auth_login(request, new_user)
+            response = Response({'serializer':serializer,'token': token.key,
+                                 'userid': user.id,
+#                                  'redirect_url': redirect_url,
+            }, template_name='home.html')
+#             response.set_cookie('authorization', token.key, max_age=MAX_AGE, expires=EXPIRES)
+#             response.set_cookie('authenticate', token.key, max_age=MAX_AGE, expires=EXPIRES)        
+        else:
+            #self.mix_panel_login(request, request.DATA, False)
+            response = Response({'serializer':serializer,
+                'errors':serializer.errors, 'status':status.HTTP_400_BAD_REQUEST})
+        
+        return response
+
+class SignoutUser(APIView):
+
+    """
+    Constraints:
+    This API is not for mobile Team.
+    """
+
+    throttle_classes = ()
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (AllowAny,)
+    parser_classes = (
+        parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,TemplateHTMLRenderer)
+    serializer_class = SignInSerializer  # AuthTokenSerializer
+    model = Token
+    template_name = 'login.html'
+    
+    def get(self, request):
+        serializer = self.serializer_class()
+        if request.user.is_authenticated():
+            auth_logout(request)
+        
+        return Response({'serializer': serializer})
+    
+    
+
