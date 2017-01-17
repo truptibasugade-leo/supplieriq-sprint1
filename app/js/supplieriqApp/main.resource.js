c4d923671b631732f1ebd81cc612901c104ee15f
trupti <truptib@leotechnosoft.net>
2017-01-17 18:08:51 +0530

    login api changes and vendor list

---
+++ app/js/supplieriqApp/main.resource.js
@@ -0,0 +1,47 @@
+(function() {
+    var app;
+
+    app = angular.module('supplieriqApp');
+
+    var actions = {        
+        'get' : {
+            method : 'GET',           
+            isArray : false
+        },
+        'save' : {
+            method : 'POST',
+            isArray : false
+        },
+        'query' : {
+            method : 'GET',
+            transformResponse : function(data) {
+                jdata = JSON.parse(data);
+                if (jdata['results']){
+                    jdata = jdata.results;
+                }
+                return jdata;
+            },
+            isArray : true
+        },
+        'remove' : {
+            method : 'DELETE'
+        },
+        'delete' : {
+            method : 'DELETE'
+        },
+
+        'list' : {
+            method : 'GET',
+            isArray : false
+        },
+
+
+    };
+
+    app.factory('GetVendorList', ['$resource',
+        function($resource) {
+            return $resource('/vendors/ ', {}, actions);
+        }]);
+    
+
+}).call(this);
