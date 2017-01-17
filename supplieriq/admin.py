0ed555f7e8585474108f7a46e3e9eac51d2c5c14
trupti <truptib@leotechnosoft.net>
2017-01-17 18:03:19 +0530

    login api changes and vendor list

--- supplieriq/admin.py
+++ supplieriq/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from supplieriq.models import Vendors,Company
 # Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    fields = ('name', )
+
+class VendorsAdmin(admin.ModelAdmin):
+    fields = ('name','email','phone','company')
+    
+
+admin.site.register(Vendors, VendorsAdmin)
+admin.site.register(Company, CompanyAdmin)
