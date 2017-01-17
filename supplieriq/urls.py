2ce4248210d2f4aea4c9ffc17001636c09620fa0
trupti <truptib@leotechnosoft.net>
2017-01-17 18:03:42 +0530

    login api changes and vendor list

--- supplieriq/urls.py
+++ supplieriq/urls.py
@@ -3,16 +3,12 @@ from django.conf.urls import patterns, url
 from supplieriq import views
 from django.views.decorators.csrf import csrf_exempt
 
-# urlpatterns = ('',
-# 
-#     url(r'^signin/$',
-#         views.ObtainAuthToken.as_view(),
-#         name='signin',
-#         ),
-# )
+
 urlpatterns = [
-    url(r'^signin/$', views.ObtainAuthToken.as_view(),
+    url(r'^$', views.ObtainAuthToken.as_view(),
         name='signin'),
-    url(r'^signout/$', views.SignoutUser.as_view(),
+    url(r'^signout', views.SignoutUser.as_view(),
         name='signout'),
+    url(r'^vendors', views.VendorsAPI.as_view(),
+        name='vendors'),
 ]
