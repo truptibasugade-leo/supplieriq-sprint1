74386952a6e45c237f7ffdb57ca3d41c2465e7ae
trupti <truptib@leotechnosoft.net>
2017-01-17 18:03:49 +0530

    login api changes and vendor list

--- supplieriq/views.py
+++ supplieriq/views.py
@@ -8,7 +8,7 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
-from rest_framework import parsers, renderers, status
+from rest_framework import parsers, renderers, status, viewsets,generics
 from rest_framework.authentication import get_authorization_header
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -20,10 +20,12 @@ import time
 from django.utils.http import cookie_date
 from django.core.cache import cache
 from django.contrib.auth.models import Group
-from supplieriq.serializers import SignInSerializer
+from supplieriq.serializers import SignInSerializer,VendorSerializer
 from django.contrib.auth.models import User
 from django.contrib import auth
 from rest_framework.renderers import TemplateHTMLRenderer
+from supplieriq.models import Vendors,Company
+from django.shortcuts import render_to_response
 
 
 class ObtainAuthToken(APIView):
@@ -98,4 +100,35 @@ class SignoutUser(APIView):
         return Response({'serializer': serializer})
     
     
+# class VendorsAPI(viewsets.ModelViewSet):
+class VendorsAPI(APIView):
+    """
+    Constraints:
+    This API is not for mobile Team.
+    """
+    
+   # serializer_class = VendorSerializer  # AuthTokenSerializer
+#     template_name = 'vendor_list.html'
+    renderer_classes = (renderers.JSONRenderer,TemplateHTMLRenderer)
+    def get(self, request,*args, **kwargs):
+        queryset = Vendors.objects.all()
+        renderer_classes = (renderers.JSONRenderer,TemplateHTMLRenderer)
+        serializer = VendorSerializer(queryset, many=True)     
+        y = []
+        for x in serializer.data:
+            y.append(dict(x))
+        return Response({'serializer': y},template_name="vendor_list.html")
+     
+    
+    '''
+    model = Vendors
+    serializer_class = VendorSerializer
+    template_name = 'vendor_list.html'
+    queryset =  Vendors.objects.all()
 
+    def list(self, request, *args, **kwargs):
+        self.object_list = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(self.object_list, many=True)
+        return Response(serializer.data)
+    
+    '''
