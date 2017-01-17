7a5b3902d59ae02528d083be29a662462717c829
trupti <truptib@leotechnosoft.net>
2017-01-17 18:08:58 +0530

    login api changes and vendor list

---
+++ app/js/supplieriqApp/supplieriqModule.js
@@ -0,0 +1,16 @@
+var app = angular.module("supplieriqApp", ['ngResource','ngRoute']);
+
+//app.config(function($routeProvider) {
+//    $routeProvider
+//    .when("/vendors", {
+//        templateUrl : "templates/vendor_list.html",
+//    })
+//    /*.when("/london", {
+//        templateUrl : "london.htm",
+//        controller : "londonCtrl"
+//    })
+//    .when("/paris", {
+//        templateUrl : "paris.htm",
+//        controller : "parisCtrl"
+//    });*/
+//});
